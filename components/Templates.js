497bb8798a3cb2b669b4c0940bf79a2dd6c3f657
David Uebelacker <david@uebelacker.ch>
2025-06-02 10:59:49 +0200

    :alien: A boss with no humor is like a job that's no fun.

--- components/Templates.js
+++ components/Templates.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React from "react";
+import getTranslations from "@/functions/getTranslations";
+import Page from "@/components/Page";
+import Section from "@/components/Section";
+import Container from "@/components/Container";
 
 export default function Templates({ templates, locale }) {
+  const t = getTranslations(locale);
+
   return (
-    <div className="blog-list">
-      {templates.map((tpl) => (
-        <a href={`/${locale}/templates/${tpl.slug}`} className="blog-card" key={tpl.slug}>
-          <h2>{tpl.subject}</h2>
-          <p>{tpl.teaser}</p>
-        </a>
-      ))}
-    </div>
+    <Page>
+      <Section>
+        <Container className="prose proese-briefe">
+          <h1>{t("templates.title")}</h1>
+          <div className="template-list">
+            {templates.map((tpl) => (
+              <a
+                href={`/${locale}/templates/${tpl.slug}`}
+                className="blog-card"
+                key={tpl.slug}
+              >
+                <h2>{tpl.subject}</h2>
+                <p>{tpl.teaser}</p>
+              </a>
+            ))}
+          </div>
+        </Container>
+      </Section>
+    </Page>
   );
 }
