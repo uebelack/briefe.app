4c7e191f36052e1229ee173713db6de5095870c2
David Übelacker <david@uebelacker.ch>
2025-06-02 20:31:33 +0200

    Redesign home page layout

--- components/Feature.js
+++ components/Feature.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Feature({ icon, title, description }) {
   return (
-    <div className="flex mb-5">
-      <div className="text-4xl mr-5 w-16 pt-2 pl-1"><i className={`fa fa-${icon}`} /></div>
-      <div className="w-full">
-        <div className="text-lg font-bold">{title}</div>
-        <div className="text-lg">{description}</div>
+    <div className="p-5 border rounded-lg shadow-sm bg-white text-center">
+      <div className="text-blue text-3xl mb-3">
+        <i className={`fa fa-${icon}`} />
       </div>
+      <div className="text-lg font-bold mb-1">{title}</div>
+      <div className="text-sm text-gray-dark">{description}</div>
     </div>
   );
 }
